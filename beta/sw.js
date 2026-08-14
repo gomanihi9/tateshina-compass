e7d440eaa832fa7a9a0e357bde0ea5c827c113b5
gomanihi9 <kopi1941@gmail.com>
2026-08-15 08:04:38 +0900

    Update sw.js

--- beta/sw.js
+++ beta/sw.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = 'tateshina-compass-v011-pwa';
+const CACHE_NAME = 'tateshina-compass-v011-webapk';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon.png'
 ];
 
 self.addEventListener('install', (event) => {
